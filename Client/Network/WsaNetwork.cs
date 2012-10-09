372e320db2c14004bf2dfd8182429c9aa84500c1
Namek <nameczanin@gmail.com>
2012-10-09 22:35:53 +0200

    Receing messages from server.

--- Client/Network/WsaNetwork.cs
+++ Client/Network/WsaNetwork.cs
@@ -6,12 +6,14 @@
     using Common;
     using Model;
     using System.Net.Sockets;
+    using System.IO;
 
     public class WsaNetwork : INetwork
     {
         #region Public members
 
         public event Action<object> OnMessageReceived;
+        public event Action<string> OnDisconnect;
 
         #endregion
 
@@ -44,7 +46,47 @@
             finally
             {
                 TimeoutObject.Set();
-            }    
+            }
+        }
+
+        private void BeginReceive(NetworkStream stream)
+        {
+            var thread = new Thread(new ParameterizedThreadStart(ReceiveThread));
+            thread.Start(stream);
+        }
+
+        private void ReceiveThread(object obj)
+        {
+            var networkStream = obj as NetworkStream;
+            var sr = new StreamReader(networkStream, System.Text.Encoding.UTF8);
+            string line = null;
+
+            try
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    // TODO Interpret message from Json format and identify message type: header or content.
+                    //      React specifically due to the message type.
+
+                    if (OnMessageReceived != null)
+                    {
+                        // TODO there should be full object
+                        OnMessageReceived.Invoke(line);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                // Connection may be forcibly closed while waiting for message.
+                // We will just catch it.
+            }
+            finally
+            {
+                if (OnDisconnect != null)
+                {
+                    OnDisconnect.Invoke("Connection ended.");
+                }
+            }
         }
     
         #endregion
@@ -84,6 +126,7 @@
                 {
                     if (IsConnectionSuccessful)
                     {
+                        BeginReceive(tcpClient.GetStream());
                         return true;
                     }
                     else
