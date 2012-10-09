e521514d36832b52d0caf6950a9a3b95036f83f4
Namek <Namek@Namek-PC>
2012-10-09 21:23:39 +0200

    Connecting with server.

--- Client/Network/WsaNetwork.cs
+++ Client/Network/WsaNetwork.cs
@@ -5,10 +5,49 @@
     using System.Threading;
     using Common;
     using Model;
+    using System.Net.Sockets;
 
     public class WsaNetwork : INetwork
     {
+        #region Public members
 
+        public event Action<object> OnMessageReceived;
+
+        #endregion
+
+        #region Internal connection handling
+
+        bool IsConnectionSuccessful = false;
+        Exception socketexception = null;
+        ManualResetEvent TimeoutObject = new ManualResetEvent(false);
+
+        private const int TIMEOUT_MILISECONDS = 3000;
+
+        private void TcpConnectCallback(IAsyncResult tcpAsyncResult)
+        {
+            try
+            {
+                IsConnectionSuccessful = false;
+                TcpClient tcpclient = tcpAsyncResult.AsyncState as TcpClient;
+             
+                if (tcpclient.Client != null)
+                {
+                    tcpclient.EndConnect(tcpAsyncResult);
+                    IsConnectionSuccessful = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                IsConnectionSuccessful = false;
+                socketexception = ex;
+            }
+            finally
+            {
+                TimeoutObject.Set();
+            }    
+        }
+    
+        #endregion
 
         #region INetwork members
 
@@ -24,16 +63,47 @@
         {
         }
 
-        public IAsyncResult BeginConnect(EndPoint remoteEP, AsyncCallback asyncCallback, object asyncState)
+        /// <summary>
+        /// Connects to the server and starts listening for messages from it.
+        /// To receive messages register OnMessageReceived event.
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        /// <param name="asyncCallback"></param>
+        /// <param name="asyncState"></param>
+        /// <returns></returns>
+        public IAsyncResult BeginConnect(string hostname, int port, AsyncCallback asyncCallback, object asyncState)
         {
+            var tcpClient = new TcpClient();
+            tcpClient.BeginConnect(hostname, port, new AsyncCallback(TcpConnectCallback), tcpClient);
+
             var ar = new AsyncResult<object>(asyncCallback, asyncState);
-            // simulate time consuming task
-            ar.BeginInvoke(() => { Thread.Sleep(500); return null; });
+            ar.BeginInvoke(() =>
+            {
+                if (TimeoutObject.WaitOne(TIMEOUT_MILISECONDS, false))
+                {
+                    if (IsConnectionSuccessful)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        throw socketexception;
+                    }
+                }
+                else
+                {
+                    tcpClient.Close();
+                    throw new TimeoutException("TimeOut Exception");
+                }
+            });
+
             return ar;
         }
         public void EndConnect(IAsyncResult asyncResult)
         {
             var ar = (AsyncResult<object>)asyncResult;
+            var res = ar.Result;
             ar.EndInvoke();
         }
         public IAsyncResult BeginLogin(string login, string password, AsyncCallback asyncCallback, object asyncState)
