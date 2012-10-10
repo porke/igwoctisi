76a298d7e8b793738a180593ad7e057333959ccd
Porke <Porke@Porke-Notebook>
2012-10-10 17:39:49 +0200

    Added GameLobbyView prototype.

--- Client/View/Lobby/GameLobbyView.cs
+++ Client/View/Lobby/GameLobbyView.cs
@@ -4,6 +4,7 @@
     using Common;
     using Input;
     using Nuclex.UserInterface;
+    using Nuclex.UserInterface.Controls;
     using Nuclex.UserInterface.Controls.Desktop;
     using State;
 
@@ -13,22 +14,46 @@
 
         protected Screen _screen;
 
+        private ListControl _messageList;
+        private InputControl _currentMessage;
+
         protected void CreateChildControls()
         {
-            var btnBeginGame = new ButtonControl
+            var btnBeginGame = new ButtonControl()
             {
                 Text = "Begin Game",
-                Bounds = new UniRectangle(new UniScalar(0.4f, 0), new UniScalar(0.475f, 0), new UniScalar(0.2f, 0), new UniScalar(0.05f, 0))
+                Bounds = new UniRectangle(new UniScalar(0.6f, 0), new UniScalar(0.2f, 0), new UniScalar(0.35f, 0), new UniScalar(0.1f, 0))
             };
-
             btnBeginGame.Pressed += BeginGame_Pressed;
 
-            _screen.Desktop.Children.AddRange(new[] { btnBeginGame });
+            var btnLeaveGame = new ButtonControl()
+            {
+                Text = "Leave Game",
+                Bounds = new UniRectangle(new UniScalar(0.6f, 0), new UniScalar(0.35f, 0), new UniScalar(0.35f, 0), new UniScalar(0.1f, 0))
+            };
+            btnLeaveGame.Pressed += LeaveGame_Pressed;
+
+            _messageList = new ListControl()
+            {
+                Bounds = new UniRectangle(new UniScalar(0.05f, 0), new UniScalar(0.5f, 0), new UniScalar(0.9f, 0), new UniScalar(0.4f, 0))
+            };
+
+            _currentMessage = new InputControl()
+            {
+                Bounds = new UniRectangle(new UniScalar(0.05f, 0), new UniScalar(0.925f, 0), new UniScalar(0.9f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _screen.Desktop.Children.AddRange(new Control[] { btnBeginGame, btnLeaveGame, _messageList, _currentMessage });
         }
 
         protected void BeginGame_Pressed(object sender, EventArgs e)
         {
             State.Game.ChangeState(new PlayState(State.Game));
+        }
+
+        protected void LeaveGame_Pressed(object sender, EventArgs e)
+        {
+            State.Game.ChangeState(new LobbyState(State.Game));
         }
 
         #endregion
@@ -69,6 +94,7 @@
         {
             State = state;
             _screen = new Screen(800, 600);
+            _screen.Desktop.Bounds = new UniRectangle(new UniScalar(0.3f, 0), new UniScalar(0.3f, 0), new UniScalar(0.4f, 0), new UniScalar(0.4f, 0));
             InputReceiver = new NuclexScreenInputReceiver(_screen, false);
 
             CreateChildControls();
