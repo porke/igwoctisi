68db1fffdf2fa795479b21eba282d3a86bcbb4ba
namek <nameczanin@gmail.com>
2012-10-13 13:39:38 +0200

    Update #2

---
+++ Client/Model/StartingData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client.Model
+{
+    public class StartingData
+    {
+        public int PlanetId { get; set; }
+
+        public StartingData(int planetId)
+        {
+            PlanetId = planetId;
+        }
+    }
+}
