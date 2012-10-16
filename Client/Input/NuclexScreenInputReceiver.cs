cf5dd0f90948b33f97b535786b28d4b03060ea11
Porke <porke90@gmail.com>
2012-10-16 20:30:21 +0200

    Added PlayerJoin, PlayerLeft and ChatMessageReceived handlers for the GameLobbyView.

---
+++ Client/Input/NuclexScreenInputReceiver.cs
@@ -0,0 +1,89 @@
+﻿namespace Client.Input
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Input;
+    using Nuclex.Input;
+    using Nuclex.UserInterface;
+    using Nuclex.UserInterface.Input;
+
+    public class NuclexScreenInputReceiver : IInputReceiver
+    {
+        #region IInputReceiver members
+
+        // commands
+        public virtual bool OnCommand(Command command)
+        {
+            Screen.InjectCommand(command);
+            return IsInputHandled;
+        }
+
+        // keyboard
+        public virtual bool OnKeyPressed(Keys key)
+        {
+            Screen.InjectKeyPress(key);
+            return IsInputHandled;
+        }
+        public virtual bool OnKeyReleased(Keys key)
+        {
+            Screen.InjectKeyRelease(key);
+            return IsInputHandled;
+        }
+        public virtual bool OnCharacter(char character)
+        {
+            Screen.InjectCharacter(character);
+            return UseModalBehavior;
+        }
+
+        // mouse
+        public virtual bool OnMouseMoved(Vector2 position)
+        {
+            Screen.InjectMouseMove(position.X, position.Y);
+            return IsInputHandled;
+        }
+        public virtual bool OnMousePressed(MouseButtons button)
+        {
+            Screen.InjectMousePress(button);
+            return IsInputHandled;
+        }
+        public virtual bool OnMouseReleased(MouseButtons button)
+        {
+            Screen.InjectMouseRelease(button);
+            return IsInputHandled;
+        }
+        public virtual bool OnMouseWheel(float ticks)
+        {
+            Screen.InjectMouseWheel(ticks);
+            return IsInputHandled;
+        }
+
+        // gamepad
+        public virtual bool OnButtonPressed(Buttons button)
+        {
+            Screen.InjectButtonPress(button);
+            return IsInputHandled;
+        }
+        public virtual bool OnButtonReleased(Buttons button)
+        {
+            Screen.InjectButtonRelease(button);
+            return IsInputHandled;
+        }
+
+        #endregion
+
+        public Screen Screen { get; protected set; }
+        public bool UseModalBehavior { get; protected set; }
+        private bool IsInputHandled
+        {
+            get
+            {
+                return Screen.IsInputCaptured || UseModalBehavior;
+            }
+        }
+
+        public NuclexScreenInputReceiver(Screen screen, bool useModalBehavior)
+        {
+            Screen = screen;
+            UseModalBehavior = useModalBehavior;
+        }
+    }
+}
---
+++ Client/State/PlayState.cs
@@ -0,0 +1,60 @@
+﻿namespace Client.State
+{
+    using System;
+    using System.Collections.Generic;
+    using Model;
+    using View.Play;
+
+    class PlayState : GameState
+    {
+        public Scene Scene { get; protected set; }
+
+        private GameViewport _gameViewport;
+        private GameHud _gameHud;
+
+        private List<UserCommand> _commands = new List<UserCommand>();
+        private Player _clientPlayer;
+
+        public PlayState(IGWOCTISI game, Map loadedMap, Player clientPlayer)
+            : base(game)
+        {
+            Scene = new Scene(loadedMap, new List<Player> { _clientPlayer});
+            _clientPlayer = clientPlayer;
+
+            _gameViewport = new GameViewport(this);
+            _gameHud = new GameHud(this);
+
+            ViewMgr.PushLayer(_gameViewport);
+            ViewMgr.PushLayer(_gameHud);
+
+            eventHandlers.Add("LeaveGame", LeaveGame);
+            eventHandlers.Add("SendOrders", SendOrders);
+            eventHandlers.Add("SelectPlanet", SelectPlanet);
+        }
+
+        #region View event handlers
+
+        private void LeaveGame(EventArgs args)
+        {
+            Client.ChangeState(new LobbyState(Game, _clientPlayer));
+        }
+
+        private void SendOrders(EventArgs args)
+        {
+            // TODO: Send orders
+            _commands.Clear();
+        }
+
+        private void SelectPlanet(EventArgs args)
+        {
+            var selectedPlanetArg = args as SelectPlanetArgs;
+            _gameHud.UpdateSelectedPlanet(selectedPlanetArg.Planet);
+        }
+
+        #endregion
+
+        #region Async network callbacks
+
+        #endregion
+    }
+}
