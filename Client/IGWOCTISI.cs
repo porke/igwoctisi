355f4cae833e35f72195ac177f78b2efea3dffef
Porke <porke90@gmail.com>
2012-10-18 22:34:31 +0200

    Added Enter handling for InputControl.

---
+++ Client/IGWOCTISI.cs
@@ -0,0 +1,27 @@
+namespace Client
+{
+    using State;
+
+    public class IGWOCTISI : GameClient
+    {
+        #region Microsoft.Xna.Framework.Game members
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            ChangeState(new MenuState(this));
+        }
+
+        #endregion
+
+        public IGWOCTISI()
+        {            
+            Content.RootDirectory = "Content";
+            GraphicsManager.PreferredBackBufferWidth = 800;
+            GraphicsManager.PreferredBackBufferHeight = 600;
+            IsMouseVisible = true;
+        }
+    }
+}
+    
---
+++ Client/Model/PlanetarySystem.cs
@@ -0,0 +1,22 @@
+﻿namespace Client.Model
+{
+    using System;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    public class PlanetarySystem
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+
+        [XmlAttribute]
+        public int FleetBonusPerTurn { get; set; }
+        
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlArray("Planets")]
+        [XmlArrayItem("PlanetId")]
+        public int[] Planets { get; set; }
+    }
+}
