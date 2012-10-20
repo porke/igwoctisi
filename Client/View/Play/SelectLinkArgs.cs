9cf4cd8ea02d3a93095be826d2060b7bff7f3433
Marko <Marko@Marko-PB>
2012-10-20 21:13:54 +0200

    T/063/K Wyswietlanie informacji o planecie

---
+++ Client/View/Play/SelectLinkArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.View.Play
+{
+	using System;
+	using Client.Model;
+
+	class SelectLinkArgs : EventArgs
+	{
+		public PlanetLink Link { get; private set; }
+
+		public SelectLinkArgs(PlanetLink selectedLink)
+		{
+			Link = selectedLink;
+		}
+	}
+}
