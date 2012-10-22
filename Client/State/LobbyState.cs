bf30d5a04ed4c38e8ac450049282fd14a3f1eb5b
namek <nameczanin@gmail.com>
2012-10-22 21:07:43 +0200

    Backed out changeset: 7b59b2de9153

---
+++ Client/State/LobbyState.cs
@@ -0,0 +1,413 @@
+﻿namespace Client.State
+{
+    using System;
+    using Client.Model;
+    using View;
+    using View.Lobby;
+
+    class LobbyState : GameState
+    {
+        private Map _map;
+        private Player _clientPlayer;
+        private SpecificGameLobbyInfo _gameLobby;
+        private bool _isHostingGame { get { return _map != null; } }
+
+        public LobbyState(IGWOCTISI game, Player player) : base(game)
+        {
+            _clientPlayer = player;
+
+            var menuBackground = new LobbyBackground(this);
+            var lobbyMenu = new MainLobbyView(this);
+
+            ViewMgr.PushLayer(menuBackground);
+            ViewMgr.PushLayer(lobbyMenu);
+
+            eventHandlers.Add("LeaveGameLobby", LeaveGameLobby);
+            eventHandlers.Add("CreateGame", CreateGame);
+            eventHandlers.Add("CancelCreateGame", CancelCreateGame);
+            eventHandlers.Add("EnterCreateGameView", EnterCreateGameView);
+            eventHandlers.Add("Logout", Logout);
+            eventHandlers.Add("JoinGame", JoinGame);
+            eventHandlers.Add("BeginGame", BeginGame);
+            eventHandlers.Add("RefreshGameList", RefreshGameList);
+            eventHandlers.Add("SendChatMessage", SendChatMessage);
+            eventHandlers.Add("KickOtherPlayer", KickOtherPlayer);
+        }
+
+        public override void OnEnter()
+        {
+            Client.Network.OnDisconnected += new Action<string>(OnDisconnected_EventHandler);
+            this.RefreshGameList(new SenderEventArgs(ViewMgr.PeekLayer()));
+        }
+
+        public override void OnExit()
+        {
+            Client.Network.OnDisconnected -= new Action<string>(OnDisconnected_EventHandler);
+        }
+
+        private void BindNetworkEvents()
+        {
+            Client.Network.OnOtherPlayerJoined += Network_OnOtherPlayerJoined;
+            Client.Network.OnOtherPlayerLeft += Network_OnOtherPlayerLeft;
+            Client.Network.OnOtherPlayerKicked += Network_OnOtherPlayerKicked;
+            Client.Network.OnChatMessageReceived += Network_OnChatMessageReceived;
+            Client.Network.OnPlayerKicked += Network_OnPlayerKicked;
+            Client.Network.OnGameStarted += Network_OnGameStarted;
+        }
+
+        private void UnbindNetworkEvents()
+        {
+            Client.Network.OnOtherPlayerJoined -= Network_OnOtherPlayerJoined;
+            Client.Network.OnOtherPlayerLeft -= Network_OnOtherPlayerLeft;
+            Client.Network.OnOtherPlayerKicked -= Network_OnOtherPlayerKicked;
+            Client.Network.OnChatMessageReceived -= Network_OnChatMessageReceived;
+            Client.Network.OnPlayerKicked -= Network_OnPlayerKicked;
+            Client.Network.OnGameStarted -= Network_OnGameStarted;
+        }
+
+        #region _view event handlers
+
+        private void LeaveGameLobby(EventArgs args)
+        {
+            Client.Network.BeginLeaveGame(OnLeaveGame, null);
+        }
+
+        private void CreateGame(EventArgs args)
+        {
+            var newGameParameters = args as CreateGameArgs;
+
+            var messageBox = new MessageBox(MessageBoxButtons.None)
+            {
+                Title = "Join Game",
+                Message = "Joining in..."
+            };
+            ViewMgr.PushLayer(messageBox);
+
+            var map = new Map(newGameParameters.MapName);
+            Client.Network.BeginCreateGame(newGameParameters.GameName, map, OnCreateGame, Tuple.Create(messageBox, newGameParameters.MapName, newGameParameters.GameName));
+        }
+
+        private void CancelCreateGame(EventArgs args)
+        {
+            ViewMgr.PopLayer();     // pop create game view
+            ViewMgr.PushLayer(new MainLobbyView(this));
+        }
+
+        private void EnterCreateGameView(EventArgs args)
+        {
+            ViewMgr.PopLayer();     // pop main lobby view
+            ViewMgr.PushLayer(new CreateGameView(this));
+        }
+
+        private void Logout(EventArgs args)
+        {
+            var ar = Client.Network.BeginLogout(OnLogout, null);
+            ar.AsyncWaitHandle.WaitOne();
+        }
+
+        private void JoinGame(EventArgs args)
+        {
+            var joinGameArgs = args as JoinGameArgs;
+
+            var messageBox = new MessageBox(MessageBoxButtons.None)
+            {
+                Title = "Join Game",
+                Message = "Joining in..."
+            };
+            ViewMgr.PushLayer(messageBox);
+            Client.Network.BeginJoinGameLobby(joinGameArgs.LobbyId, OnJoinLobby, messageBox);
+        }
+
+        private void BeginGame(EventArgs args)
+        {
+            var messageBox = new MessageBox(MessageBoxButtons.None)
+            {
+                Title = "Begin Game",
+                Message = "Starting game, please wait..."
+            };
+            ViewMgr.PushLayer(messageBox);
+
+            Client.Network.BeginStartGame(OnGameStarted, messageBox);
+        }
+
+        private void RefreshGameList(EventArgs args)
+        {
+            var messageBox = new MessageBox(MessageBoxButtons.None)
+            {
+                Title = "Loading Main Lobby",
+                Message = "Downloading game list..."
+            };            
+
+            ViewMgr.PushLayer(messageBox);
+
+            var mainLobbyView = (args as SenderEventArgs).Sender;
+            Client.Network.BeginGetGameList(OnGetGameList, Tuple.Create(mainLobbyView as MainLobbyView, messageBox));
+        }
+
+        private void SendChatMessage(EventArgs args)
+        {
+            var msgArgs = args as ChatMessageArgs;
+            Client.Network.BeginSendChatMessage(msgArgs.Message, (res) => { try { Client.Network.EndSendChatMessage(res); } catch { } }, null);
+        }
+
+        private void KickOtherPlayer(EventArgs args)
+        {
+            var kickArgs = args as KickPlayerArgs;
+            Client.Network.BeginKickPlayer(kickArgs.Username, OnKickPlayer, kickArgs.Username);
+        }
+
+        #endregion
+
+        #region Network async callbacks
+
+        private void OnLogout(IAsyncResult result)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                try
+                {
+                    Client.Network.EndLogout(result);
+                }
+                catch { }
+                finally
+                {
+                    Client.Network.BeginDisconnect(OnDisconnect, null);
+                }
+            });
+        }
+                
+        private void OnGetGameList(IAsyncResult result)
+        {
+            var data = (Tuple<MainLobbyView, MessageBox>)result.AsyncState;
+            var mainLobbyView = data.Item1;
+            var messageBox = data.Item2;
+
+            InvokeOnMainThread(obj =>
+            {
+                try
+                {
+                    var gameNames = Client.Network.EndGetGameList(result);
+                    mainLobbyView.RefreshGameList(gameNames);
+                    ViewMgr.PopLayer(); // MessageBox
+                }
+                catch (Exception exc)
+                {
+                    messageBox.Buttons = MessageBoxButtons.OK;
+                    messageBox.Message = exc.Message;
+                    messageBox.OkPressed += (sender, e) => { ViewMgr.PopLayer(); };
+                }
+            });
+        }
+
+        private void OnJoinLobby(IAsyncResult result)
+        {
+            var messageBox = result.AsyncState as MessageBox;
+            BindNetworkEvents();
+            
+            InvokeOnMainThread(obj =>
+            {
+                try
+                {
+                    _gameLobby = Client.Network.EndJoinGameLobby(result);                     
+
+                    ViewMgr.PopLayer(); // MessageBox
+                    ViewMgr.PopLayer(); // MainLobbyView
+
+                    var gameLobbyView = new GameLobbyView(this, false);
+                    gameLobbyView.RefreshPlayerList(_gameLobby.Players);
+                    ViewMgr.PushLayer(gameLobbyView);
+                }
+                catch (Exception exc)
+                {
+                    messageBox.Buttons = MessageBoxButtons.OK;
+                    messageBox.Message = exc.Message;
+                    messageBox.OkPressed += (sender, e) => { ViewMgr.PopLayer(); };
+                }
+            });
+        }
+
+        private void OnLeaveGame(IAsyncResult result)
+        {
+            try
+            {
+                Client.Network.EndLeaveGame(result);
+                _gameLobby = null;
+
+                UnbindNetworkEvents();
+            }
+            catch { }
+            finally
+            {
+                InvokeOnMainThread(arg =>
+                {
+                    ViewMgr.PopLayer(); // pop game lobby
+                    ViewMgr.PushLayer(new MainLobbyView(this));
+                });
+            }
+        }
+
+        private void OnKickPlayer(IAsyncResult result)
+        {
+            try
+            {
+                Client.Network.EndKickPlayer(result);
+                // Don't remove the player manually from lists.
+                // Server should send GamePlayerLeft message to anyone.
+            }
+            catch
+            {
+            }
+        }
+
+        private void OnCreateGame(IAsyncResult result)
+        {
+            BindNetworkEvents();
+
+            InvokeOnMainThread(obj =>
+            {
+                var data = result.AsyncState as Tuple<MessageBox, string, string>;
+                var messageBox = data.Item1;
+                string mapName = data.Item2;
+                string gameName = data.Item3;
+
+                try
+                {
+                    Client.Network.EndCreateGame(result);
+
+                    _map = new Map(mapName);
+                    _gameLobby = new SpecificGameLobbyInfo(gameName, _clientPlayer);
+
+                    ViewMgr.PopLayer();     // pop MessageBox
+                    ViewMgr.PopLayer();     // pop main lobby window
+
+                    var gameLobbyView = new GameLobbyView(this, true);
+                    gameLobbyView.RefreshPlayerList(_gameLobby.Players);
+                    ViewMgr.PushLayer(gameLobbyView);
+                }
+                catch (Exception exc)
+                {
+                    messageBox.Buttons = MessageBoxButtons.OK;
+                    messageBox.OkPressed += (sender, e) => { ViewMgr.PopLayer(); };
+                    messageBox.Message = exc.Message;
+                }
+            });            
+        }
+
+        private void OnGameStarted(IAsyncResult result)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                var messageBox = result.AsyncState as MessageBox;
+
+                try
+                {
+                    Client.Network.EndStartGame(result);
+                }
+                catch (Exception exc)
+                {
+                    messageBox.Buttons = MessageBoxButtons.OK;
+                    messageBox.OkPressed += (sender, e) => { ViewMgr.PopLayer(); ViewMgr.PopLayer(); };
+                    messageBox.Message = exc.Message;
+                }
+            });
+        }
+
+        private void OnDisconnect(IAsyncResult result)
+        {
+            try
+            {
+                Client.Network.EndDisconnect(result);
+            }
+            catch { }
+            finally { }
+        }
+
+        private void OnDisconnected_EventHandler(string reason)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                UnbindNetworkEvents();
+
+                var menuState = new MenuState(Game);
+                Client.ChangeState(menuState);
+                menuState.HandleViewEvent("OnDisconnected", new MessageBoxArgs("Disconnection", "You were disconnected from the server."));
+            });
+        }
+
+        #endregion
+
+        #region Network event handlers
+
+        private void Network_OnOtherPlayerJoined(string username, DateTime time)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                var gameLobbyView = ViewMgr.PeekLayer() as GameLobbyView;
+                _gameLobby.AddPlayer(username);
+                gameLobbyView.RefreshPlayerList(_gameLobby.Players);
+                gameLobbyView.AddHostMessage(username + " joined.", time.ToString("H:mm"));
+            });
+        }
+
+        private void Network_OnOtherPlayerLeft(string username, DateTime time)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                var gameLobbyView = ViewMgr.PeekLayer() as GameLobbyView;
+                _gameLobby.RemovePlayer(username);
+                gameLobbyView.RefreshPlayerList(_gameLobby.Players);
+                gameLobbyView.AddHostMessage(username + " left.", time.ToString("H:mm"));
+            });
+        }
+
+        private void Network_OnOtherPlayerKicked(string username, DateTime time)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                var gameLobbyView = ViewMgr.PeekLayer() as GameLobbyView;
+                _gameLobby.RemovePlayer(username);
+                gameLobbyView.RefreshPlayerList(_gameLobby.Players);
+                gameLobbyView.AddHostMessage(username + " have been kicked by host.", time.ToString("H:mm"));
+            });
+        }
+
+        private void Network_OnChatMessageReceived(ChatMessage message)
+        {
+            InvokeOnMainThread(obj =>
+            {
+                var gameLobbyView = ViewMgr.PeekLayer() as GameLobbyView;
+                gameLobbyView.ChatMessageReceived(obj as ChatMessage);
+            }, message);
+        }
+        
+        void Network_OnPlayerKicked()
+        {
+            InvokeOnMainThread(obj =>
+            {
+                UnbindNetworkEvents();
+
+                ViewMgr.PopLayer(); // GameLobbyView
+                ViewMgr.PushLayer(new MainLobbyView(this));
+
+                var messageBox = new MessageBox(MessageBoxButtons.OK)
+                {
+                    Title = "Out of Game",
+                    Message = "You were kicked out from Game Lobby."
+                };
+                messageBox.OkPressed += (sender, e) => { ViewMgr.PopLayer(); };
+                ViewMgr.PushLayer(messageBox);
+            });
+        }
+
+        void Network_OnGameStarted(Map map)
+        {
+            if (!_isHostingGame) InvokeOnMainThread(obj =>
+            {
+                UnbindNetworkEvents();
+                Game.ChangeState(new PlayState(Game, _map, _clientPlayer));
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ Client/View/Play/GameHud.cs
@@ -0,0 +1,288 @@
+﻿namespace Client.View.Play
+{
+    using System;
+    using Client.Model;
+    using Common;
+    using Input;
+    using Nuclex.UserInterface;
+    using Nuclex.UserInterface.Controls;
+    using Nuclex.UserInterface.Controls.Desktop;
+    using State;
+    using System.Collections.Generic;
+
+    class GameHud : BaseView
+    {
+        #region Protected members
+
+        private ListControl _orderList;
+        private LabelControl _playerNameValue;
+        private LabelControl _fleetCountValue;
+        private LabelControl _fleetIncomeValue;
+        
+        private LabelControl _selectedPlanetName;
+        private LabelControl _selectedPlanetBaseIncome;
+        private LabelControl _selectedPlanetFleetCount;
+
+        private LabelControl _timer;
+
+        protected void CreateChildControls()
+        {
+            #region Orders section
+
+            var ordersHeader = new LabelControl("Orders")
+            {                
+                Bounds = new UniRectangle(new UniScalar(0.78f, 0), new UniScalar(0.22f, 0), new UniScalar(0.34f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _orderList = new ListControl()
+            {
+                SelectionMode = ListSelectionMode.Single,
+                Bounds = new UniRectangle(new UniScalar(0.65f, 0), new UniScalar(0.3f, 0), new UniScalar(0.34f, 0), new UniScalar(0.45f, 0))
+            };
+
+            #endregion
+
+            #region Fleet values
+
+            var playerNameDesc = new LabelControl("Player:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.0f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _playerNameValue = new LabelControl("")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.0f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            var fleetCountDesc = new LabelControl("Deployable fleets:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.05f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _fleetCountValue = new LabelControl("0")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.05f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            var fleetIncomeDesc = new LabelControl("Fleets per turn:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.1f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _fleetIncomeValue = new LabelControl("0")
+            {                
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.1f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            #endregion
+
+            #region Game buttons
+
+            var btnLeaveGame = new ButtonControl()
+            {
+                Text = "Leave",
+                Bounds = new UniRectangle(new UniScalar(0.02f, 0), new UniScalar(0.93f, 0), new UniScalar(0.1f, 0), new UniScalar(0.05f, 0))
+            };
+            btnLeaveGame.Pressed += LeaveGame_Pressed;
+
+            var btnSendOrders = new ButtonControl()
+            {
+                Text = "End turn",
+                Bounds = new UniRectangle(new UniScalar(0.14f, 0), new UniScalar(0.93f, 0), new UniScalar(0.1f, 0), new UniScalar(0.05f, 0))
+            };
+            btnSendOrders.Pressed += SendOrders_Pressed;
+
+            #endregion
+
+            #region Order buttons
+
+            var btnMoveOrderUp = new ButtonControl()
+            {
+                Text = "Up",
+                Bounds = new UniRectangle(new UniScalar(0.65f, 0), new UniScalar(0.77f, 0), new UniScalar(0.1f, 0), new UniScalar(0.05f, 0))
+            };
+            btnMoveOrderUp.Pressed += MoveOrderUp_Pressed;
+
+            var btnMoveOrderDown = new ButtonControl()
+            {
+                Text = "Down",
+                Bounds = new UniRectangle(new UniScalar(0.77f, 0), new UniScalar(0.77f, 0), new UniScalar(0.1f, 0), new UniScalar(0.05f, 0))
+            };
+            btnMoveOrderDown.Pressed += MoveOrderDown_Pressed;
+
+            var btnDeleteOrder = new ButtonControl()
+            {
+                Text = "Delete",
+                Bounds = new UniRectangle(new UniScalar(0.89f, 0), new UniScalar(0.77f, 0), new UniScalar(0.1f, 0), new UniScalar(0.05f, 0))
+            };
+            btnDeleteOrder.Pressed += DeleteOrder_Pressed;
+
+            #endregion
+
+            #region Selected planet data
+
+            var selectedPlanetDesc = new LabelControl("Selected planet:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.2f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _selectedPlanetName = new LabelControl("None")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.2f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            var planetFleetIncomeDesc = new LabelControl("Fleet income:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.25f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _selectedPlanetBaseIncome = new LabelControl("-")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.25f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            var planetStationedFleets = new LabelControl("Fleet count:")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.01f, 0), new UniScalar(0.3f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            _selectedPlanetFleetCount = new LabelControl("-")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.175f, 0), new UniScalar(0.3f, 0), new UniScalar(0.3f, 0), new UniScalar(0.1f, 0))
+            };
+
+            #endregion
+
+            #region Timer
+
+            _timer = new LabelControl("0:00")
+            {
+                Bounds = new UniRectangle(new UniScalar(0.89f, 0), new UniScalar(0.0f, 0), new UniScalar(0.1f, 0), new UniScalar(0.07f, 0))               
+            };
+
+            #endregion
+
+            screen.Desktop.Children.AddRange(
+                new Control[] 
+                {
+                    ordersHeader, 
+                    _orderList, 
+                    
+                    _playerNameValue,
+                    playerNameDesc,
+                    _fleetIncomeValue, 
+                    _fleetCountValue, 
+                    fleetCountDesc,
+                    fleetIncomeDesc,
+                    
+                    btnDeleteOrder, 
+                    btnLeaveGame, 
+                    btnSendOrders,
+                    btnMoveOrderDown,
+                    btnMoveOrderUp,
+                    
+                    _selectedPlanetName,
+                    _selectedPlanetBaseIncome,
+                    _selectedPlanetFleetCount,
+                    planetFleetIncomeDesc,
+                    selectedPlanetDesc,
+                    planetStationedFleets,
+
+                    _timer
+                });
+        }        
+
+        #endregion
+
+        #region Event handlers
+
+        private void LeaveGame_Pressed(object sender, EventArgs e)
+        {
+            state.HandleViewEvent("LeaveGame", e);
+        }
+
+        private void SendOrders_Pressed(object sender, EventArgs e)
+        {
+            state.HandleViewEvent("SendOrders", e);
+        }
+
+        private void MoveOrderUp_Pressed(object sender, EventArgs e)
+        {
+            // TODO: move order up the list
+        }
+
+        private void MoveOrderDown_Pressed(object sender, EventArgs e)
+        {
+            // TODO: move order down the list
+        }
+
+        private void DeleteOrder_Pressed(object sender, EventArgs e)
+        {
+            if (_orderList.SelectedItems.Count > 0)
+            {
+                var firstItem = _orderList.SelectedItems[0];
+                _orderList.Items.RemoveAt(firstItem);
+                _orderList.SelectedItems.RemoveAt(0);
+
+                if (_orderList.Items.Count > 0)
+                {
+                    if (firstItem - 1 > 0) 
+                    {
+                        firstItem--;
+                    }
+
+                    _orderList.SelectedItems.Add(firstItem);
+                }
+
+                // TODO update the logic (fleet count, etc.)
+            }
+        }
+
+        #endregion
+
+        #region Update requests
+
+        public void UpdateSelectedPlanet(Planet planet)
+        {
+            _selectedPlanetName.Text = planet.Name;
+            _selectedPlanetBaseIncome.Text = Convert.ToString(planet.BaseUnitsPerTurn);
+            _selectedPlanetFleetCount.Text = Convert.ToString(planet.NumFleetsPresent);
+        }
+
+        public void UpdateClientPlayerFleetData(Player player)
+        {
+            _playerNameValue.Text = player.Username;
+            _fleetCountValue.Text = Convert.ToString(player.DeployableFleets);
+            _fleetIncomeValue.Text = Convert.ToString(player.FleetIncomePerTurn);
+        }
+
+        public void UpdateTimer(int secondsLeft)
+        {
+            int mins = secondsLeft / 60;
+            int secs = secondsLeft % 60;
+
+            // Display Timer in format 0:00
+            _timer.Text = mins.ToString() + (secs < 10 ? ":0" : ":") + secs.ToString();
+        }
+
+        public void UpdateOrders(List<UserCommand> commands)
+        {
+            _orderList.Items.Clear();
+            foreach (var cmd in commands)
+            {
+                _orderList.Items.Add(string.Format("Deploy {0} fleets to planet {1}", cmd.UnitCount, cmd.TargetPlanet.Name));   
+            }            
+        }
+
+        #endregion
+
+        public GameHud(PlayState state) : base(state)
+        {
+            IsLoaded = true;
+            IsTransparent = true;            
+            InputReceiver = new NuclexScreenInputReceiver(screen, false);            
+
+            CreateChildControls();
+        }
+    }
+}
