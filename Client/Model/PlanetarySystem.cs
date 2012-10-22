78d7ae2313eeedc0fc7ed212f31613a5bc116008
namek <nameczanin@gmail.com>
2012-10-22 14:38:33 +0200

    Go to play state for non-host players.

--- Client/Model/PlanetarySystem.cs
+++ Client/Model/PlanetarySystem.cs
@@ -1,22 +1,28 @@
 ﻿namespace Client.Model
 {
     using System;
+    using System.Runtime.Serialization;
     using System.Xml.Serialization;
 
-    [Serializable]
+    [Serializable] //XML
+    [DataContract] //Json
     public class PlanetarySystem
     {
         [XmlAttribute]
+        [DataMember]
         public int Id { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public int FleetBonusPerTurn { get; set; }
         
         [XmlAttribute]
+        [DataMember]
         public string Name { get; set; }
 
         [XmlArray("Planets")]
         [XmlArrayItem("PlanetId")]
+        [DataMember]
         public int[] Planets { get; set; }
     }
 }
