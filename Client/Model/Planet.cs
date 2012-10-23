6e08ba05eee3c8d3065931649c4f8b8f84769d69
namek <nameczanin@gmail.com>
2012-10-24 00:51:04 +0200

    Change map state when round ends. Use information received from server.

--- Client/Model/Planet.cs
+++ Client/Model/Planet.cs
@@ -4,48 +4,57 @@
     using System.Xml.Serialization;
     using Client.Renderer;
     using Newtonsoft.Json;
+    using System.Runtime.Serialization;
 
     [Serializable]
+    [DataContract]
     public class Planet
     {
         [XmlAttribute]
+        [DataMember]
         public string Name { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public int BaseUnitsPerTurn { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public int Id { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public float X { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public float Y { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public float Z { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public float Radius { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public string Diffuse { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public string Clouds { get; set; }
 
         [XmlAttribute]
+        [DataMember]
         public string CloudsAlpha { get; set; }
-
-        [JsonIgnore]
+        
+        
         public int NumFleetsPresent { get; set; }
-
-        [JsonIgnore]
         public PlanetVisual Visual { get; set; }
 
-        [JsonIgnore]
-        [XmlIgnore]
+        [XmlIgnore, JsonIgnore]
         public Player Owner { get; set; }
     }
 }
