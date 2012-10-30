1e2e220cc38b9dbeb8e14f295d611e691b3f2641
namek <nameczanin@gmail.com>
2012-10-30 18:08:16 +0100

    Moved Color enum to PlayerColor out of Player class.

--- Client/Renderer/XnaRenderer.cs
+++ Client/Renderer/XnaRenderer.cs
@@ -1,163 +1,163 @@
 ﻿namespace Client.Renderer
 {
-    using System;
-    using System.Linq;
-    using Client.Common;
-    using Microsoft.Xna.Framework;
-    using Microsoft.Xna.Framework.Graphics;
-    using Model;
+	using System;
+	using System.Linq;
+	using Client.Common;
+	using Microsoft.Xna.Framework;
+	using Microsoft.Xna.Framework.Graphics;
+	using Model;
 
-    public class XnaRenderer : IRenderer
-    {
-        #region Protected members
+	public class XnaRenderer : IRenderer
+	{
+		#region Protected members
 
 		protected const float HoverAmbient = 1.0f;
 		protected const float LinkJointSize = 15.0f;
 		protected readonly Vector2 NameOffset = new Vector2(0.0f, 25.0f);
 		protected readonly Vector2 FleetsOffset = new Vector2(0.0f, 42.0f);
-        protected readonly Vector2 FleetIncomeOffset = new Vector2(21.0f, 42.0f);
-        protected readonly Vector2 OwnerNameOffset = new Vector2(0.0f, 59.0f);
+		protected readonly Vector2 FleetIncomeOffset = new Vector2(21.0f, 42.0f);
+		protected readonly Vector2 OwnerNameOffset = new Vector2(0.0f, 59.0f);
 		protected SpriteBatch _spriteBatch;
 		protected SpriteFont _fontHud;
-        protected Effect _fxLinks, _fxPlanet;
-        protected VertexBuffer _sphereVB;
+		protected Effect _fxLinks, _fxPlanet;
+		protected VertexBuffer _sphereVB;
 
-        private SimpleCamera _camera = new SimpleCamera();
+		private SimpleCamera _camera = new SimpleCamera();
 
-        protected void InitializeMapVisual(Map map)
-        {
-            var vertices = new VertexPositionColor[map.Links.Count * 2];
-            var color = Color.LightGreen;
+		protected void InitializeMapVisual(Map map)
+		{
+			var vertices = new VertexPositionColor[map.Links.Count * 2];
+			var color = Color.LightGreen;
 
-            for (var i = 0; i < map.Links.Count; ++i)
-            {
-                var link = map.Links[i];
-                var sourcePlanet = map.Planets.First(x => x.Id == link.SourcePlanet);
-                var targetPlanet = map.Planets.First(x => x.Id == link.TargetPlanet);
+			for (var i = 0; i < map.Links.Count; ++i)
+			{
+				var link = map.Links[i];
+				var sourcePlanet = map.Planets.First(x => x.Id == link.SourcePlanet);
+				var targetPlanet = map.Planets.First(x => x.Id == link.TargetPlanet);
 
-                vertices[2 * i + 0] = new VertexPositionColor(new Vector3(sourcePlanet.X, sourcePlanet.Y, sourcePlanet.Z), color);
-                vertices[2 * i + 1] = new VertexPositionColor(new Vector3(targetPlanet.X, targetPlanet.Y, targetPlanet.Z), color);
-            }
+				vertices[2 * i + 0] = new VertexPositionColor(new Vector3(sourcePlanet.X, sourcePlanet.Y, sourcePlanet.Z), color);
+				vertices[2 * i + 1] = new VertexPositionColor(new Vector3(targetPlanet.X, targetPlanet.Y, targetPlanet.Z), color);
+			}
 
-            var visual = new MapVisual();
-            visual.LinksVB = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
-            visual.LinksVB.SetData(vertices);
+			var visual = new MapVisual();
+			visual.LinksVB = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+			visual.LinksVB.SetData(vertices);
 
-            map.Visual = visual;
-        }
-        protected void InitializePlanetVisual(Planet planet)
-        {
-            var contentMgr = Client.Content;
-            var visual = new PlanetVisual();
-            var random = new Random(Guid.NewGuid().GetHashCode());
+			map.Visual = visual;
+		}
+		protected void InitializePlanetVisual(Planet planet)
+		{
+			var contentMgr = Client.Content;
+			var visual = new PlanetVisual();
+			var random = new Random(Guid.NewGuid().GetHashCode());
 
-            visual.Period = (float)(random.NextDouble() * 10.0 + 5.0);
-            visual.Yaw = (float)(random.NextDouble() * MathHelper.TwoPi);
-            visual.Pitch = (float)(random.NextDouble() * MathHelper.TwoPi);
-            visual.Roll = (float)(random.NextDouble() * MathHelper.TwoPi);
-            
-            if (!string.IsNullOrEmpty(planet.Diffuse))
-            {
-                visual.DiffuseTexture = contentMgr.Load<Texture2D>(planet.Diffuse);
-            }
-            if (!string.IsNullOrEmpty(planet.Clouds))
-            {
-                visual.CloudsTexture = contentMgr.Load<Texture2D>(planet.Clouds);
-            }
-            if (!string.IsNullOrEmpty(planet.CloudsAlpha))
-            {
-                visual.CloudsAlphaTexture = contentMgr.Load<Texture2D>(planet.CloudsAlpha);
-            }
+			visual.Period = (float)(random.NextDouble() * 10.0 + 5.0);
+			visual.Yaw = (float)(random.NextDouble() * MathHelper.TwoPi);
+			visual.Pitch = (float)(random.NextDouble() * MathHelper.TwoPi);
+			visual.Roll = (float)(random.NextDouble() * MathHelper.TwoPi);
+			
+			if (!string.IsNullOrEmpty(planet.Diffuse))
+			{
+				visual.DiffuseTexture = contentMgr.Load<Texture2D>(planet.Diffuse);
+			}
+			if (!string.IsNullOrEmpty(planet.Clouds))
+			{
+				visual.CloudsTexture = contentMgr.Load<Texture2D>(planet.Clouds);
+			}
+			if (!string.IsNullOrEmpty(planet.CloudsAlpha))
+			{
+				visual.CloudsAlphaTexture = contentMgr.Load<Texture2D>(planet.CloudsAlpha);
+			}
 
-            planet.Visual = visual;
-        }
+			planet.Visual = visual;
+		}
 
-        #endregion
+		#endregion
 
-        #region IRenderer members
+		#region IRenderer members
 
-        public bool RaySphereIntersection(Vector2 screenPosition, Vector3 position, float radius)
-        {
-            var ray = _camera.GetRay(GraphicsDevice.Viewport, new Vector3(screenPosition, 0));
-            return ray.Intersects(new BoundingSphere(position, radius)) != null;
-        }
+		public bool RaySphereIntersection(Vector2 screenPosition, Vector3 position, float radius)
+		{
+			var ray = _camera.GetRay(GraphicsDevice.Viewport, new Vector3(screenPosition, 0));
+			return ray.Intersects(new BoundingSphere(position, radius)) != null;
+		}
 		public bool RayLinkIntersection(Vector2 screenPosition, Vector3 linkSource, Vector3 linkTarget)
 		{
 			var ray = _camera.GetRay(GraphicsDevice.Viewport, new Vector3(screenPosition, 0));
 			return ray.Intersects(new BoundingSphere((linkSource + linkTarget) / 2.0f, LinkJointSize)) != null;
 		}
 
-        public void Initialize(GameClient client)
-        {
-            Client = client;
-            GraphicsDevice = Client.GraphicsDevice;
+		public void Initialize(GameClient client)
+		{
+			Client = client;
+			GraphicsDevice = Client.GraphicsDevice;
 
-            var contentMgr = Client.Content;
+			var contentMgr = Client.Content;
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 			_fontHud = contentMgr.Load<SpriteFont>("Fonts\\HUD");
-            _fxLinks = contentMgr.Load<Effect>("Effects\\Links");
-            _fxPlanet = contentMgr.Load<Effect>("Effects\\Planet");
+			_fxLinks = contentMgr.Load<Effect>("Effects\\Links");
+			_fxPlanet = contentMgr.Load<Effect>("Effects\\Planet");
 
-            var vertices = Utils.SphereVertices(3).Select(x => new Vertex(x.Position, x.Normal, Color.LightGreen, x.TextureCoordinate)).ToArray();
+			var vertices = Utils.SphereVertices(3).Select(x => new Vertex(x.Position, x.Normal, Color.LightGreen, x.TextureCoordinate)).ToArray();
 			_sphereVB = new VertexBuffer(GraphicsDevice, Vertex.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
-            _sphereVB.SetData(vertices);
-        }
-        public void Release()
-        {
-            Client = null;
-        }
-        public void Draw(Scene scene, double delta, double time)
-        {
-            _camera.Update(delta);
+			_sphereVB.SetData(vertices);
+		}
+		public void Release()
+		{
+			Client = null;
+		}
+		public void Draw(Scene scene, double delta, double time)
+		{
+			_camera.Update(delta);
 
-            var map = scene.Map;
-            
-            #region Links
+			var map = scene.Map;
+			
+			#region Links
 
-            _camera.Apply(_fxLinks, Matrix.Identity);
+			_camera.Apply(_fxLinks, Matrix.Identity);
 
-            if (map.Visual == null)
-            {
-                InitializeMapVisual(map);
-            }
+			if (map.Visual == null)
+			{
+				InitializeMapVisual(map);
+			}
 
 			_fxLinks.Parameters["Ambient"].SetValue(0.0f);
-            foreach (var pass in _fxLinks.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.SetVertexBuffer(map.Visual.LinksVB);
-                GraphicsDevice.DrawPrimitives(PrimitiveType.LineList, 0, map.Visual.LinksVB.VertexCount / 2);
-            }
+			foreach (var pass in _fxLinks.CurrentTechnique.Passes)
+			{
+				pass.Apply();
+				GraphicsDevice.SetVertexBuffer(map.Visual.LinksVB);
+				GraphicsDevice.DrawPrimitives(PrimitiveType.LineList, 0, map.Visual.LinksVB.VertexCount / 2);
+			}
 
-            #endregion
+			#endregion
 
-            #region Planets
+			#region Planets
 
-            foreach (var planet in map.Planets)
-            {
-                if (planet.Visual == null)
-                {
-                    InitializePlanetVisual(planet);
-                }
-                var visual = planet.Visual;
+			foreach (var planet in map.Planets)
+			{
+				if (planet.Visual == null)
+				{
+					InitializePlanetVisual(planet);
+				}
+				var visual = planet.Visual;
 
 				_fxPlanet.Parameters["Ambient"].SetValue(scene.SelectedPlanet == planet.Id || scene.HoveredPlanet == planet.Id ? HoverAmbient : 0.0f);
-                _fxPlanet.Parameters["Diffuse"].SetValue(visual.DiffuseTexture);
-                _fxPlanet.Parameters["Clouds"].SetValue(visual.CloudsTexture);
-                _fxPlanet.Parameters["CloudsAlpha"].SetValue(visual.CloudsAlphaTexture);
+				_fxPlanet.Parameters["Diffuse"].SetValue(visual.DiffuseTexture);
+				_fxPlanet.Parameters["Clouds"].SetValue(visual.CloudsTexture);
+				_fxPlanet.Parameters["CloudsAlpha"].SetValue(visual.CloudsAlphaTexture);
 
-                var localWorld = Matrix.CreateScale(planet.Radius) * 
-                            Matrix.CreateRotationY((float)time / visual.Period * MathHelper.TwoPi) *
-                            Matrix.CreateFromYawPitchRoll(visual.Yaw, visual.Pitch, visual.Roll) *
-                            Matrix.CreateTranslation(planet.X, planet.Y, planet.Z);
+				var localWorld = Matrix.CreateScale(planet.Radius) * 
+							Matrix.CreateRotationY((float)time / visual.Period * MathHelper.TwoPi) *
+							Matrix.CreateFromYawPitchRoll(visual.Yaw, visual.Pitch, visual.Roll) *
+							Matrix.CreateTranslation(planet.X, planet.Y, planet.Z);
 
-                _camera.Apply(_fxPlanet, localWorld);                
-                foreach (var pass in _fxPlanet.CurrentTechnique.Passes)
-                {
-                    pass.Apply();
-                    GraphicsDevice.SetVertexBuffer(_sphereVB);
-                    GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, _sphereVB.VertexCount / 3);
-                }
+				_camera.Apply(_fxPlanet, localWorld);                
+				foreach (var pass in _fxPlanet.CurrentTechnique.Passes)
+				{
+					pass.Apply();
+					GraphicsDevice.SetVertexBuffer(_sphereVB);
+					GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, _sphereVB.VertexCount / 3);
+				}
 
 				if (scene.SelectedPlanet == planet.Id)
 				{
@@ -182,9 +182,9 @@
 						}
 					}
 				}
-            }
+			}
 			
-            #endregion
+			#endregion
 
 			#region Planets info
 
@@ -194,11 +194,11 @@
 			{
 				var planetScreen = _camera.Project(GraphicsDevice.Viewport, new Vector3(planet.X, planet.Y, planet.Z));
 				var fleetText = planet.NumFleetsPresent.ToString();
-                var fleetIncome = string.Format("+{0}", planet.BaseUnitsPerTurn);
-                var ownerName = planet.Owner != null ? planet.Owner.Username : string.Empty;
+				var fleetIncome = string.Format("+{0}", planet.BaseUnitsPerTurn);
+				var ownerName = planet.Owner != null ? planet.Owner.Username : string.Empty;
 				var nameSize = _fontHud.MeasureString(planet.Name);
 				var fleetsSize = _fontHud.MeasureString(fleetText);
-                var ownerColor = planet.Owner != null ? planet.Owner.Color : Color.Gray;
+				var ownerColor = planet.Owner != null ? planet.Owner.XnaColor : Color.Gray;
 
 				var nameScreen = new Vector2(planetScreen.X - nameSize.X / 2.0f, planetScreen.Y - nameSize.Y / 2.0f);
 				_spriteBatch.DrawString(_fontHud, planet.Name, nameScreen + NameOffset, Color.Yellow);
@@ -206,9 +206,9 @@
 				var fleetsScreen = new Vector2(planetScreen.X - fleetsSize.X / 2.0f, planetScreen.Y - fleetsSize.Y / 2.0f);
 				_spriteBatch.DrawString(_fontHud, fleetText, fleetsScreen + FleetsOffset, Color.Yellow);
 
-                _spriteBatch.DrawString(_fontHud, fleetIncome, fleetsScreen + FleetIncomeOffset, Color.Yellow);
+				_spriteBatch.DrawString(_fontHud, fleetIncome, fleetsScreen + FleetIncomeOffset, Color.Yellow);
 
-                _spriteBatch.DrawString(_fontHud, ownerName, nameScreen + OwnerNameOffset, ownerColor);
+				_spriteBatch.DrawString(_fontHud, ownerName, nameScreen + OwnerNameOffset, ownerColor);
 			}
 
 			_spriteBatch.End();
@@ -216,11 +216,11 @@
 			#endregion
 		}
 
-        #endregion
+		#endregion
 
-        public SimpleCamera GetCamera() { return _camera; }
+		public SimpleCamera GetCamera() { return _camera; }
 
-        public GameClient Client { get; protected set; }
-        public GraphicsDevice GraphicsDevice { get; protected set; }
-    }
+		public GameClient Client { get; protected set; }
+		public GraphicsDevice GraphicsDevice { get; protected set; }
+	}
 }
