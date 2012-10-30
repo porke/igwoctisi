5583aed7e24b9414456f0825dffe920af33c4815
Porke <porke90@gmail.com>
2012-10-30 15:42:08 +0100

    Merge

---
+++ Client/Common/NuclexExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Nuclex.UserInterface.Controls;
+using Client.View;
+using Client.View.Animations;
+
+
+namespace Client.Common
+{
+	public static class NuclexExtensions
+	{
+		public static Animation<Control> Animate(this Control control, BaseView view)
+		{
+			var animationMgr = view.ViewMgr.AnimationManager;
+			var animation = new Wait<Control>(control, animationMgr, 0);
+			animationMgr.AddAnimation(animation);
+			return animation;
+		}
+	}
+}
