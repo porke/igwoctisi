97b0a2ee5d5a4d6daada9fc1cc5a857c7e88909d
Marko <rogosz.marek@gmail.com>
2012-11-07 21:02:16 +0100

    ViewMgr moved back to GameState. No comments...;(

--- Client/State/GameState.cs
+++ Client/State/GameState.cs
@@ -10,6 +10,7 @@
     {
         public GameClient Client { get; protected set; }
         public IGWOCTISI Game { get; protected set; }
+		public ViewManager ViewMgr { get; protected set; }
 
         public delegate void MessageQueueFunc(object args);
         private ConcurrentQueue<Tuple<MessageQueueFunc, object>> _messageQueue = new ConcurrentQueue<Tuple<MessageQueueFunc, object>>();
@@ -18,18 +19,37 @@
         {
             Game = client as IGWOCTISI;
             Client = client;
+			ViewMgr = new ViewManager();
         }
 
         public virtual void OnEnter()
         {
             // Implementation not required
         }
-
         public virtual void OnExit()
         {
             // Implementation not required
-        }
+		}
+		public virtual void Update(double delta, double time)
+		{
+			ViewMgr.Update(delta, time);
 
+			if (!_messageQueue.IsEmpty)
+			{
+				Tuple<MessageQueueFunc, object> front;
+				if (_messageQueue.TryDequeue(out front))
+				{
+					front.Item1(front.Item2);
+				}
+			}
+		}
+		public virtual void Draw(double delta, double time)
+		{
+			var graphicsDevice = Client.GraphicsDevice;
+
+			graphicsDevice.Clear(ClearOptions.DepthBuffer | ClearOptions.Target, Color.Black, 1f, 0);
+			ViewMgr.Draw(delta, time);
+		}
         /// <summary>
         /// The function is to be called from the async callback thread. 
         /// It will invoke the given delegate in the main update thread.
@@ -37,28 +57,6 @@
         public void InvokeOnMainThread(MessageQueueFunc functionToInvoke, object arg = null)
         {
             _messageQueue.Enqueue(new Tuple<MessageQueueFunc, object>(functionToInvoke, arg));
-        }
-
-        public virtual void OnUpdate(double delta, double time)
-        {
-			Client.ViewMgr.Update(delta, time);
-
-            if (!_messageQueue.IsEmpty)
-            {
-                Tuple<MessageQueueFunc, object> front;
-                if (_messageQueue.TryDequeue(out front))
-                {
-                    front.Item1(front.Item2);
-                }
-            }
-        }
-
-        public virtual void BeforeDraw(double delta, double time)
-        {
-            var graphicsDevice = Client.GraphicsDevice;
-
-            graphicsDevice.Clear(ClearOptions.DepthBuffer | ClearOptions.Target, Color.Black, 1f, 0);
-			Client.ViewMgr.Draw(delta, time);
         }
     }
 }
