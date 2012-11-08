52404be8a653cc0f140543e2f387f152876ab7fc
Porke <porke90@gmail.com>
2012-11-08 23:33:37 +0100

    Fixed missing file. The PlanetState message is now correct.

---
+++ Client/Model/NewRoundInfo.cs
@@ -0,0 +1,71 @@
+﻿namespace Client.Model
+{
+	using System.Collections.Generic;
+    using System.Linq;
+	using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Data being received from server in RoundStart packet.
+    /// </summary>
+    [DataContract]
+    public class NewRoundInfo
+    {
+        [DataMember]
+        public List<string> Players { get; set; }
+
+        [DataMember]
+        public List<PlanetState> Map { get; set; }
+
+        [DataMember]
+        public List<int> Tech { get; set; }
+
+        [DataMember]
+        public int FleetsToDeploy { get; set; }
+
+        /// <summary>
+        /// Number of seconds.
+        /// </summary>
+        [DataMember]
+        public int RoundTime { get; set; }
+
+        [DataContract]
+        public class PlanetState
+        {
+            [DataMember]
+            public int PlanetId { get; set; }
+
+            [DataMember]
+            public string Player { get; set; }
+
+            [DataMember]
+            public int Fleets { get; set; }
+        }
+
+        /// <summary>
+        /// Gets owner of the planet.
+        /// </summary>
+        /// <param name="planetId">id of planet</param>
+        /// <param name="out_username">returned username of player owning the planet</param>
+        /// <returns>true if out_username param was set, otherwise false</returns>
+        public bool TryFindPlanetOwner(int planetId, ref string out_username)
+        {
+            var ownerName = Map.First(ps => ps.PlanetId == planetId).Player;
+            if (!string.IsNullOrEmpty(ownerName))
+            {
+				out_username = ownerName;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets planet state.
+        /// </summary>
+        /// <returns>planet state (fleet count)</returns>
+        public PlanetState FindPlanetState(int planetId)
+        {
+            return Map.First(p => p.PlanetId.Equals(planetId));
+        }
+    }
+}
---
+++ Client/Renderer/SimpleCamera.cs
@@ -0,0 +1,67 @@
+﻿namespace Client.Renderer
+{
+    using Client.Common.AnimationSystem;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class SimpleCamera : IMovable
+    {
+        private const float Velocity = 250.0f;
+
+        public Vector3 TranslationDirection { get; set; }
+		public Vector3 Position { get; set; }
+		public Vector3 LookAt { get; set; }
+
+        private Matrix _world;
+		private Matrix _view
+		{
+			get { return Matrix.CreateLookAt(Position, LookAt, Vector3.Up); }
+		}
+        private Matrix _projection;
+        
+        public SimpleCamera(GraphicsDevice graphicsDevice)
+        {
+            _world = Matrix.Identity;
+			Position = Vector3.Backward * -1000;
+			LookAt = Vector3.Zero;
+            _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), graphicsDevice.Viewport.AspectRatio, 1, 10000);
+        }
+
+        public void Update(double delta)
+        {
+			Position += (float)delta * TranslationDirection * 100;
+			LookAt += (float)delta * TranslationDirection * 100;
+        }
+
+        public void ApplyToEffect(Effect effect, Matrix localWorld)
+        {
+            if (effect is BasicEffect)
+            {
+                var basicEffect = effect as BasicEffect;
+				basicEffect.World = localWorld * _world;
+                basicEffect.View = _view;
+                basicEffect.Projection = _projection;
+            }
+            else
+            {
+				effect.Parameters["World"].SetValue(localWorld * _world);
+                effect.Parameters["View"].SetValue(_view);
+                effect.Parameters["Projection"].SetValue(_projection);
+            }
+        }
+
+        public Ray GetRay(Viewport viewport, Vector3 pointOnScreen)
+        {
+            var pointNear = new Vector3(pointOnScreen.X, pointOnScreen.Y, 0);
+            var pointFar = new Vector3(pointOnScreen.X, pointOnScreen.Y, 1);
+            var pointNearUnproj = viewport.Unproject(pointNear, _projection, _view, _world);
+            var pointFarUnproj = viewport.Unproject(pointFar, _projection, _view, _world);
+            var dir = Vector3.Normalize(pointFarUnproj - pointNearUnproj);
+            return new Ray(pointNearUnproj, dir);
+        }
+		public Vector3 Project(Viewport viewport, Vector3 worldVector)
+		{
+			return viewport.Project(worldVector, _projection, _view, _world);
+		}
+    }
+}
