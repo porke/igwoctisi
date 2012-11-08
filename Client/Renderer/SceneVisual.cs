9312910fddcb9c32ee83e1fa9e619fadfb3135ab
namek <nameczanin@gmail.com>
2012-11-08 21:40:57 +0100

    Joined move and attack animations into one common sequence.

--- Client/Renderer/SceneVisual.cs
+++ Client/Renderer/SceneVisual.cs
@@ -1,83 +1,78 @@
 ﻿namespace Client.Renderer
 {
-    using System;
-    using System.Collections.Generic;
-    using Client.Common.AnimationSystem;
-    using Client.Model;
-    using Microsoft.Xna.Framework.Content;
-    using Client.View.Play.Animations;
+	using System;
+	using System.Collections.Generic;
+	using Client.Common.AnimationSystem;
+	using Client.Model;
+	using Microsoft.Xna.Framework.Content;
+	using Client.View.Play.Animations;
+	using System.Threading;
 
-    public sealed class SceneVisual
-    {
-        public SimpleCamera Camera { get; set; }
+	public sealed class SceneVisual
+	{
+		public SimpleCamera Camera { get; set; }
 
-        private Scene Scene { get; set; }
-        private readonly AnimationManager AnimationManager;
-        private readonly List<Spaceship> _spaceships = new List<Spaceship>();
-        private readonly List<Spaceship> _spaceshipsToAdd = new List<Spaceship>();
-        private readonly List<Spaceship> _spaceshipsToRemove = new List<Spaceship>();
+		private Scene Scene { get; set; }
+		private readonly AnimationManager AnimationManager;
+		private readonly List<Spaceship> _spaceships = new List<Spaceship>();
+		private readonly List<Spaceship> _spaceshipsToAdd = new List<Spaceship>();
+		private readonly List<Spaceship> _spaceshipsToRemove = new List<Spaceship>();
 
-        public SceneVisual(Scene scene, ICollection<PlayerColor> colors, ContentManager Content, AnimationManager AnimationManager)
-        {
-            Scene = scene;
-            Spaceship.SetupColorPools(colors, Content, AnimationManager);
-            this.AnimationManager = AnimationManager;
+		public SceneVisual(Scene scene, ICollection<PlayerColor> colors, ContentManager Content, AnimationManager AnimationManager)
+		{
+			Scene = scene;
+			Spaceship.SetupColorPools(colors, Content, AnimationManager);
+			this.AnimationManager = AnimationManager;
 
-            // Install handlers
-            scene.AnimDeploys += new Action<IList<Tuple<Planet, int, Action>>>(Animation_Deploys);
-            scene.AnimMoves += new Action<IList<Tuple<Planet, Planet, int, Action>>>(Animation_Moves);
-            scene.AnimAttacks+=new Action<List<Tuple<SimulationResult,Action>>>(Animation_Attacks);
-        }
+			// Install handlers
+			scene.AnimDeploys += new Action<IList<Tuple<Planet, int, Action>>>(Animation_Deploys);
+			scene.AnimMovesAndAttacks += new Action<List<Tuple<Planet, Planet, SimulationResult, Action<SimulationResult>>>>(Animation_MovesAndAttacks);
+		}
 
-        void Animation_Deploys(IList<Tuple<Planet, int, Action>> deploys)
-        {
-            this.AnimateDeploys(AnimationManager, Camera, deploys);
-        }
+		void Animation_Deploys(IList<Tuple<Planet, int, Action>> deploys)
+		{
+			this.AnimateDeploys(AnimationManager, Camera, deploys);
+		}
 
-        void Animation_Moves(IList<Tuple<Planet, Planet, int, Action>> moves)
-        {
-            this.AnimateMoves(moves, AnimationManager, Camera);
-        }
+		void Animation_MovesAndAttacks(IList<Tuple<Planet, Planet, SimulationResult, Action<SimulationResult>>> movesAndAttacks)
+		{
+			this.AnimateMovesAndAttacks(movesAndAttacks, AnimationManager, Camera);
+		}
 
-        void Animation_Attacks(IList<Tuple<SimulationResult, Action>> attacks)
-        {
-            this.AnimateAttacks(attacks, AnimationManager, Camera);
-        }
+		internal void AddSpaceship(Spaceship ship)
+		{
+			lock (_spaceshipsToAdd)
+			{
+				_spaceshipsToAdd.Add(ship);
+			}
+		}
 
-        internal void AddSpaceship(Spaceship ship)
-        {
-            lock (_spaceshipsToAdd)
-            {
-                _spaceshipsToAdd.Add(ship);
-            }
-        }
+		internal void Draw(double delta, double time)
+		{
+			// Update spaceship list
+			lock (_spaceshipsToAdd)
+			{
+				// It is more efficient to use that type of loop than foreach/ForEach.
+				for (int i = 0, n = _spaceshipsToAdd.Count; i < n; ++i)
+					_spaceships.Add(_spaceshipsToAdd[i]);
 
-        internal void Draw(double delta, double time)
-        {
-            // Update spaceship list
-            lock (_spaceshipsToAdd)
-            {
-                // It is more efficient to use that type of loop than foreach/ForEach.
-                for (int i = 0, n = _spaceshipsToAdd.Count; i < n; ++i)
-                    _spaceships.Add(_spaceshipsToAdd[i]);
+				_spaceshipsToAdd.Clear();
+			}
 
-                _spaceshipsToAdd.Clear();
-            }
+			// TODO Draw planets and links (and particles?)
 
-            // TODO Draw planets and links (and particles?)
-
-            // Draw spaceships
-            lock (_spaceshipsToRemove)
-            {
-                foreach (var ship in _spaceships)
-                {
-                    if (ship.Visible)
-                        ship.Draw(Camera, delta, time);
-                    else
-                        _spaceshipsToRemove.Add(ship);
-                }
-                _spaceshipsToRemove.Clear();
-            }
-        }
-    }
+			// Draw spaceships
+			lock (_spaceshipsToRemove)
+			{
+				foreach (var ship in _spaceships)
+				{
+					if (ship.Visible)
+						ship.Draw(Camera, delta, time);
+					else
+						_spaceshipsToRemove.Add(ship);
+				}
+				_spaceshipsToRemove.Clear();
+			}
+		}
+	}
 }
---
+++ Client/View/Play/Animations/MoveAndAttack.cs
@@ -0,0 +1,75 @@
+﻿namespace Client.View.Play.Animations
+{
+	using System;
+	using System.Collections.Generic;
+	using Client.Common.AnimationSystem;
+	using Client.Model;
+	using Client.Renderer;
+	using System.Threading;
+	using System.Diagnostics;
+	
+	public static class MoveAndAttack
+	{
+		public static void AnimateMovesAndAttacks(this SceneVisual scene,
+			IList<Tuple<Planet, Planet, SimulationResult, Action<SimulationResult>>> movesAndAttacks,
+			AnimationManager animationManager, SimpleCamera camera)
+		{
+			ThreadPool.QueueUserWorkItem(obj =>
+			{
+				ManualResetEvent waiter = new ManualResetEvent(false);
+				foreach (var tpl in movesAndAttacks)
+				{
+					var sourcePlanet = tpl.Item1;
+					var targetPlanet = tpl.Item2;
+					var simResult = tpl.Item3;
+					var callback = tpl.Item4;
+
+					waiter.Reset();
+					if (simResult.Type == SimulationResult.MoveType.Move)
+					{
+						AnimateMove(sourcePlanet, targetPlanet, simResult, scene, animationManager, camera, waiter);
+					}
+					else
+					{
+						Debug.Assert(simResult.Type == SimulationResult.MoveType.Attack);
+						AnimateAttack(sourcePlanet, targetPlanet, simResult, scene, animationManager, camera, waiter);
+					}
+					waiter.WaitOne();
+					callback.Invoke(simResult);
+				}
+			});
+		}
+
+		private static void AnimateMove(Planet sourcePlanet, Planet targetPlanet, SimulationResult simResult,
+			SceneVisual scene, AnimationManager animationManager, SimpleCamera camera, ManualResetEvent waiter)
+		{
+			var ship = Spaceship.Acquire(sourcePlanet.Owner.Color);
+			scene.AddSpaceship(ship);
+
+			ship.Position = sourcePlanet.Position;
+			ship.Animate(animationManager)
+				.MoveTo(targetPlanet.Position, 2, Interpolators.AccelerateDecelerate())
+				.AddCallback(s =>
+				{
+					Spaceship.Recycle(s);
+					waiter.Set();
+				});
+		}
+
+		private static void AnimateAttack(Planet sourcePlanet, Planet targetPlanet, SimulationResult simResult,
+			SceneVisual scene, AnimationManager animationManager, SimpleCamera camera, ManualResetEvent waiter)
+		{
+			var ship = Spaceship.Acquire(sourcePlanet.Owner.Color);
+			scene.AddSpaceship(ship);
+
+			ship.Position = sourcePlanet.Position;
+			ship.Animate(animationManager)
+				.MoveTo(targetPlanet.Position, 2, Interpolators.AccelerateDecelerate())
+				.AddCallback(s =>
+				{
+					Spaceship.Recycle(s);
+					waiter.Set();
+				});
+		}
+	}
+}
