5c9d5c6a3a4a9aabf452000f0d46781bd81691ea
namek <nameczanin@gmail.com>
2012-11-09 02:07:49 +0100

    Added CompoundAnimation. Changed IMovable to ITransformable. Developed InterpolateTo animation.

---
+++ Client/Model/Player.cs
@@ -0,0 +1,161 @@
+﻿namespace Client.Model
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using Microsoft.Xna.Framework;
+    
+    [DataContract]
+    public class Player
+    {
+        #region Public Fields
+
+        [DataMember]
+        public string Username { get; private set; }
+
+		public List<Planet> OwnedPlanets
+		{
+			set
+			{
+				_ownedPlanets.Clear();
+				_ownedPlanets.AddRange(value);
+			}
+		}
+		public int TechPoints { get; set; }
+        public int DeployableFleets { get; set; }
+        public bool CanDeployFleets { get { return DeployableFleets > 0; } }
+        public int FleetIncomePerTurn
+        {
+            get
+            {
+                int fleets = 0;
+                foreach (var planet in _ownedPlanets)
+                {
+                    fleets += planet.BaseUnitsPerTurn;
+                }
+
+                return fleets;
+            }
+        }
+        public List<UserCommand> Commands { get; private set; }
+        public PlayerColor Color { get; set; }
+
+        #endregion
+
+        #region Private Fields
+
+        private readonly List<Planet> _ownedPlanets = new List<Planet>();
+
+        #endregion
+
+        public Player(string username, PlayerColor color)
+        {
+            Username = username;
+            Color = color;
+            Commands = new List<UserCommand>();
+        }
+
+        #region Command operations
+
+        public void ClearCommandList()
+        {
+            Commands.Clear();
+        }
+        public void DeployFleet(Planet planet)
+        {
+            var command = Commands.Find(cmd => cmd.Type == UserCommand.CommandType.Deploy && cmd.TargetId == planet.Id);
+            if (command == null)
+            {
+                command = new UserCommand(planet, 1);
+                Commands.Add(command);
+            }
+            else
+            {
+                command.FleetCount++;
+            }
+
+            planet.NumFleetsPresent++;
+            DeployableFleets--;
+        }
+        public void UndeployFleet(Planet planet)
+        {
+            var command = Commands.Find(cmd => cmd.Type == UserCommand.CommandType.Deploy && cmd.TargetId == planet.Id);
+            command.FleetCount--;
+            planet.NumFleetsPresent--;
+            DeployableFleets++;
+
+            if (command.FleetCount == 0)
+            {
+                Commands.Remove(command);
+            }
+        }
+        public void MoveFleet(Planet source, Planet target)
+        {
+			// Check if movement between these two planets exists, if so update it, otherwise, create a new command
+            var command = Commands.Find(cmd => cmd.SourceId == source.Id && cmd.TargetId == target.Id);
+            if (command == null)
+            {
+                command = new UserCommand(source, target);
+                command.FleetCount = 1;
+                Commands.Add(command);
+            }
+            else
+            {
+                command.FleetCount++;
+            }
+
+            source.FleetChange--;
+			target.FleetChange++;
+        }
+        public void RevertFleetMove(Planet source, Planet target)
+        {
+            var targetCommand = Commands.Find(cmd => cmd.SourceId == source.Id && cmd.TargetId == target.Id);
+            targetCommand.FleetCount--;
+            source.FleetChange++;
+			target.FleetChange--;
+
+            if (targetCommand.FleetCount == 0)
+            {
+                Commands.Remove(targetCommand);
+            }
+        }
+        public void DeleteCommand(int commandIndex)
+        {
+            var deletedCommand = Commands[commandIndex];
+
+            // Remove dependant commands
+            // ex. a move dependant on an earlier deploy
+            if (!deletedCommand.CanRevert())
+            {
+                var dependantCommands = Commands.FindAll(cmd => cmd.Type != UserCommand.CommandType.Deploy && cmd.SourceId == deletedCommand.TargetId);
+
+                foreach (var item in dependantCommands)
+                {
+                    item.Revert();
+                    Commands.Remove(item);
+                }
+            }
+
+            deletedCommand.Revert();
+            Commands.RemoveAt(commandIndex);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Add planet to the player. Planet's owner is changed to this player.
+        /// </summary>
+        /// <returns>true if player didn't own the planet before</returns>
+        public bool TryAssignPlanet(Planet planet)
+        {
+            // If player doesn't own the planet then assign it to him.
+            if (!_ownedPlanets.Exists(p => p.Id == planet.Id))
+            {
+                _ownedPlanets.Add(planet);
+                planet.Owner = this;
+                return true;
+            }
+
+            return false;
+        }    
+    }
+}
---
+++ Client/Renderer/SimpleCamera.cs
@@ -0,0 +1,78 @@
+﻿namespace Client.Renderer
+{
+    using Client.Common.AnimationSystem;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class SimpleCamera : ITransformable
+    {
+        private const float Velocity = 250.0f;
+
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float Z { get; set; }
+
+		public float RotationX { get; set; }
+		public float RotationY { get; set; }
+		public float RotationZ { get; set; }
+
+		public float ScaleX { get; set; }
+		public float ScaleY { get; set; }
+		public float ScaleZ { get; set; }
+
+        public Vector3 TranslationDirection { get; set; }
+		public Vector3 LookAt { get; set; }
+
+        private Matrix _world;
+		private Matrix _view
+		{
+			get { return Matrix.CreateLookAt(this.GetPosition(), LookAt, Vector3.Up); }
+		}
+        private Matrix _projection;
+        
+        public SimpleCamera(GraphicsDevice graphicsDevice)
+        {
+            _world = Matrix.Identity;
+			this.SetPosition(Vector3.Backward * -1000);
+			LookAt = Vector3.Zero;
+            _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), graphicsDevice.Viewport.AspectRatio, 1, 10000);
+        }
+
+        public void Update(double delta)
+        {
+			this.SetPosition(this.GetPosition() + (float)delta * TranslationDirection * 100);
+			LookAt += (float)delta * TranslationDirection * 100;
+        }
+
+        public void ApplyToEffect(Effect effect, Matrix localWorld)
+        {
+            if (effect is BasicEffect)
+            {
+                var basicEffect = effect as BasicEffect;
+				basicEffect.World = localWorld * _world;
+                basicEffect.View = _view;
+                basicEffect.Projection = _projection;
+            }
+            else
+            {
+				effect.Parameters["World"].SetValue(localWorld * _world);
+                effect.Parameters["View"].SetValue(_view);
+                effect.Parameters["Projection"].SetValue(_projection);
+            }
+        }
+
+        public Ray GetRay(Viewport viewport, Vector3 pointOnScreen)
+        {
+            var pointNear = new Vector3(pointOnScreen.X, pointOnScreen.Y, 0);
+            var pointFar = new Vector3(pointOnScreen.X, pointOnScreen.Y, 1);
+            var pointNearUnproj = viewport.Unproject(pointNear, _projection, _view, _world);
+            var pointFarUnproj = viewport.Unproject(pointFar, _projection, _view, _world);
+            var dir = Vector3.Normalize(pointFarUnproj - pointNearUnproj);
+            return new Ray(pointNearUnproj, dir);
+        }
+		public Vector3 Project(Viewport viewport, Vector3 worldVector)
+		{
+			return viewport.Project(worldVector, _projection, _view, _world);
+		}
+    }
+}
