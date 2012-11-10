f99515e1f49d90ea631bd4127067ca218799084a
Porke <porke90@gmail.com>
2012-11-10 20:12:00 +0100

    Working on the stats window. Fixed a bug (exception when creating and entering a game for the second time).

---
+++ Client/Model/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.Model
+{
+	using System.Runtime.Serialization;
+
+	[DataContract]
+    public class ChatMessage
+    {
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Time { get; set; }
+    }
+}
