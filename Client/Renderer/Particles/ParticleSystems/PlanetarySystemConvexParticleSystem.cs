cc90350772043f5fd7ec8f0554e742e17327423a
namek <nameczanin@gmail.com>
2012-11-15 19:54:20 +0100

    Made improvements in planetary system convex particles.

--- Client/Renderer/Particles/ParticleSystems/PlanetarySystemConvexParticleSystem.cs
+++ Client/Renderer/Particles/ParticleSystems/PlanetarySystemConvexParticleSystem.cs
@@ -7,13 +7,24 @@
 	using Microsoft.Xna.Framework;
 	using Microsoft.Xna.Framework.Content;
 	using Microsoft.Xna.Framework.Graphics;
+	using Client.Model;
 
 
-	class PlanetarySystemConvexParticleSystem : ParticleSystem
+	public class PlanetarySystemConvexParticleSystem : ParticleSystem
 	{
-		public PlanetarySystemConvexParticleSystem(Game game, ContentManager content)
+		private Vector3[] _keyPoints;
+		private int _index;
+		private float _animationAmount;
+		private Vector3 _previousPosition;
+
+		private const float _timeFactor = 2.5f;
+
+
+		public PlanetarySystemConvexParticleSystem(Game game, ContentManager content, Point3[] keyPoints)
 			: base(game, content)
 		{
+			_keyPoints = keyPoints.Select(p => p.ToVector3()).ToArray();
+			_index = 0;
 		}
 
 		public void SetColor(Color color)
@@ -26,17 +37,17 @@
 		{
 			settings.TexturePath = @"Textures\Effects\Flare";
 
-			settings.MaxParticles = 2400;
+			settings.MaxParticles = 4400;
 
-			settings.Duration = TimeSpan.FromSeconds(2);
+			settings.Duration = TimeSpan.FromSeconds(8);
 
-			settings.DurationRandomness = 1;
+			settings.DurationRandomness = 0.1f;
 
 			settings.MinHorizontalVelocity = 0;
-			settings.MaxHorizontalVelocity = 15;
+			settings.MaxHorizontalVelocity = 5;
 
-			settings.MinVerticalVelocity = -10;
-			settings.MaxVerticalVelocity = 10;
+			settings.MinVerticalVelocity = 0;
+			settings.MaxVerticalVelocity = 5;
 
 			settings.Gravity = new Vector3(0, 0, 0);
 
@@ -51,6 +62,38 @@
 
 			// Use additive blending.
 			settings.BlendState = BlendState.Additive;
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+
+			int n = _keyPoints.Length;
+			int index = _index;
+
+			var p1 = _keyPoints[index];
+			if (index < n - 1) ++index; else index = 0;
+			var position = Vector3.CatmullRom(
+				p1,
+				_keyPoints[index < n ? index++ : (index = 0)],
+				_keyPoints[index < n ? index++ : (index = 0)],
+				_keyPoints[index < n ? index : 0],
+				_animationAmount
+			);
+			var velocity = Vector3.Normalize(_previousPosition - position);
+			
+			AddParticle(position, velocity);
+
+			_previousPosition = position;
+			_animationAmount += (float)gameTime.ElapsedGameTime.TotalSeconds * _timeFactor;
+			if (_animationAmount >= 1.0f)
+			{
+				_animationAmount = 0;
+
+				_index++;
+				if (_index >= n)
+					_index = 0;
+			}
 		}
 	}
 }
--- Client/Renderer/PlanetarySystemVisual.cs
+++ Client/Renderer/PlanetarySystemVisual.cs
@@ -9,12 +9,12 @@
 
 	public sealed class PlanetarySystemVisual
 	{
-		public ParticleSystem ParticleSystem { get; private set; }
+		public PlanetarySystemConvexParticleSystem ParticleSystem { get; private set; }
 
 		public Color Color
 		{
 			get { return _color; } 
-			set { _color = value; UpdateColor(); }
+			set { _color = value; ParticleSystem.SetColor(value); }
 		}
 
 		public bool Visible
@@ -22,36 +22,18 @@
 			get { return ParticleSystem.Visible; }
 			set { ParticleSystem.Visible = value; }
 		}
-
-		private Point3[] _keyPoints;
-		private int _lastKeyPoint;
+		
 		private Color _color;
 
 
 		public PlanetarySystemVisual(PlanetarySystem planetarySystem,Game Game, ContentManager Content, Point3[] keyPoints)
 		{
-			_keyPoints = keyPoints;
-			_lastKeyPoint = 0;
-			ParticleSystem = new PlanetarySystemConvexParticleSystem(Game, Content);
+			ParticleSystem = new PlanetarySystemConvexParticleSystem(Game, Content, keyPoints);
 		}
 
 		public void Update(GraphicsDevice device, ICamera camera, double delta, double time)
 		{
-			int index = _lastKeyPoint;
-			var position1 = _keyPoints[index].ToVector3();
-			index = index + 1 == _keyPoints.Length ? 0 : index + 1;
-			var position2 = _keyPoints[index].ToVector3();
-			var velocity = position2 - position1;
-
 			ParticleSystem.SetCamera(camera.GetView(), camera.Projection);
-			ParticleSystem.AddParticle(position1, velocity);
-			_lastKeyPoint = index;
-		}
-
-		private void UpdateColor()
-		{
-			ParticleSystem.SetMinColor(_color);
-			ParticleSystem.SetMaxColor(_color);
 		}
 	}
 }
