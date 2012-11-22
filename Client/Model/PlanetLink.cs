cf783113b23b0c158150f7492c255d754d4baf7c
namek <nameczanin@gmail.com>
2012-11-22 04:57:28 +0100

    Added opposite links for arrowheaded indicators picking.

--- Client/Model/PlanetLink.cs
+++ Client/Model/PlanetLink.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Runtime.Serialization;
     using System.Xml.Serialization;
+	using Newtonsoft.Json;
 
     [Serializable] //XML
     [DataContract] //Json
@@ -15,5 +16,22 @@
         [XmlAttribute("TargetId")]
         [DataMember] //Json
         public int TargetPlanet { get; set; }
+
+		[JsonIgnore]
+		[IgnoreDataMember]
+		public PlanetLink OppositeLink { get; set; }
+
+		[OnDeserialized]
+		public void OnJsonDeserialized(StreamingContext context)
+		{
+			OnXmlDeserialized();
+		}
+
+		public void OnXmlDeserialized()
+		{
+			OppositeLink = new PlanetLink();
+			OppositeLink.SourcePlanet = TargetPlanet;
+			OppositeLink.TargetPlanet = SourcePlanet;
+		}
     }
 }
